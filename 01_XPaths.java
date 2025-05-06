ae0554c71fdf155de309270cc5f0036b24c9819a
Anurag-elitx <kumaranurag50534@gmail.com>
2025-05-06 16:16:30 +0530

    Update 01_XPaths.java

--- 01_XPaths.java
+++ 01_XPaths.java
@@ -1,1 +1,31 @@
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class AliExpressXPathDemo {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.aliexpress.com");
+        driver.manage().window().maximize();
+
+        WebElement searchBar = driver.findElement(By.xpath("//input[@type='search']"));
+        WebElement searchButton = driver.findElement(By.xpath("//button[contains(@class,'search-button')]"));
+        WebElement categoryLink = driver.findElement(By.xpath("//a[contains(text(),'Electronics')]"));
+        WebElement bannerImage = driver.findElement(By.xpath("//img[@alt='Main Banner']"));
+        WebElement footer = driver.findElement(By.xpath("//footer"));
+
+        searchBar.sendKeys("smart watch");
+        searchButton.click();
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        driver.quit();
+    }
+}
